cd4b2b3064cbad11422ed437d51a9e08ecd9e219
Carlos Rafael <carlosk2gunn@hotmail.com>
2023-12-10 21:34:57 -0300

    ✨ feat: add escalonador 100% accepted

--- trabalho2/escalador_fcfs.cpp
+++ trabalho2/escalador_fcfs.cpp
@@ -5,117 +5,43 @@ using namespace std;
 #define all(x) begin(x), end(x)
 #define sz(x) (int)(x).size()
 typedef long long ll;
-typedef pair<int, string> pis;
-typedef vector<int> vi;
+typedef list<int> vi;
+typedef pair<int, vi> pis;
 
-void simulacao(queue<pis> &fila)
+void simula(list<pis> &fila)
 {
   int tempoTotal = 0;
 
   while (!fila.empty())
   {
-    bool foiInterrompido = false;
     pis processo = fila.front();
-    fila.pop();
+    int instrucao = processo.second.front();
 
-    // for (int i = 0; processo.second.size(); i++)
-    // {
-    //   char instrucao = processo.second[i];
-    //   if (instrucao == '0')
-    //   {
-    //     tempoTotal += 10;
-    //     foiInterrompido = false;
-    //   }
-    //   else if (instrucao == '1')
-    //   {
-    //     processo.second += '0';
-    //     fila.push(processo);
-    //     foiInterrompido = true;
-    //     break;
-    //   }
-    // }
-    for (char instrucao : processo.second)
-    {
-      string sub = processo.second.substr(1);
-      // cout << processo.first << " -> Atual " << processo.second << " Sub: " << sub << endl;
-      if (instrucao == '0')
-      {
-        tempoTotal += 10;
-        foiInterrompido = false;
-      }
-      else if (instrucao == '1')
-      {
-        processo.second = '0' + sub;
-        fila.push(processo);
-        foiInterrompido = true;
-        break;
-      }
-    }
-    if (!foiInterrompido)
-      cout << processo.first << " (" << tempoTotal << ")" << endl;
-  }
-}
-
-void simulacao2(queue<pis> &fila)
-{
-  int tempoTotal = 0;
-
-  while (!fila.empty())
-  {
-    bool foiInterrompido = false;
-    pis processo = fila.front();
-    char instrucao = processo.second[0];
-
-    cout << processo.first << " -> " << processo.second << endl;
-
-    if (instrucao == '0')
+    if (instrucao == 0)
     {
       tempoTotal += 10;
-      foiInterrompido = false;
-      processo.second.erase(0, 1);
+      if (processo.second.size() > 1)
+      {
+        processo.second.pop_front();
+        fila.erase(fila.begin());
+        fila.push_front(make_pair(processo.first, processo.second));
+      }
+      else
+      {
+        fila.pop_front();
+        cout << processo.first << " (" << tempoTotal << ")" << endl;
+        continue;
+      }
     }
-    else if (instrucao == '1')
+    else if (instrucao == 1)
     {
-      processo.second[0] = '0';
-      foiInterrompido = true;
-      fila.pop();
-    }
+      processo.second.erase(processo.second.begin());
+      processo.second.push_front(0);
 
-    if (processo.second.size() <= 0)
-      fila.pop();
-
-    if (foiInterrompido and processo.second.size() > 0)
-      fila.push(processo);
-    else
-      cout << processo.first << " (" << tempoTotal << ")" << endl;
-  }
-}
-
-void simulacao3(queue<pis> &fila)
-{
-  int tempoTotal = 0;
-
-  while (!fila.empty())
-  {
-    pis processo = fila.front();
-    char instrucao = processo.second[0];
-
-    cout << processo.first << " -> " << processo.second << endl;
-
-    if (instrucao == '0')
-    {
-      tempoTotal += 10;
-      processo.second.erase(0, 1);
-    }
-    else if (instrucao == '1')
-    {
-      processo.second[0] = '0';
-      fila.pop();
-      fila.push(processo);
+      fila.pop_front();
+      fila.push_back(processo);
       continue;
     }
-    if (processo.second.size() <= 0)
-      fila.pop();
   }
 }
 
@@ -124,7 +50,7 @@ int main()
   int qtd_processos;
   cin >> qtd_processos;
 
-  queue<pis> fila_execucao;
+  list<pis> fila_execucao;
 
   vector<int> qtd_instrucoes_p_processo(qtd_processos);
   rep(i, 0, qtd_processos)
@@ -137,25 +63,18 @@ int main()
 
   rep(i, 0, qtd_processos)
   {
-    string instrucao_completa = "";
+    list<int> instrucoes;
     rep(j, 0, qtd_instrucoes_p_processo[i])
     {
-      char instrucao;
+      int instrucao;
       cin >> instrucao;
-      instrucao_completa += instrucao;
+
+      instrucoes.push_back(instrucao);
     }
-    fila_execucao.push(make_pair(i + 1, instrucao_completa));
+    fila_execucao.push_back(make_pair(i + 1, instrucoes));
   }
 
-  simulacao3(fila_execucao);
-
-  // rep(i, 0, qtd_processos)
-  // {
-  //   cout << "Processo " << fila_execucao.front().first << ": " << fila_execucao.front().second << endl;
-  //   fila_execucao.pop();
-  // }
-
-  // simulacao(fila_execucao);
+  simula(fila_execucao);
 
   return 0;
 }
